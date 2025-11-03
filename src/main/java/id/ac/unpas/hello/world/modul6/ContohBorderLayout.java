5299c6b7426a14deb816a8f01c504debf82ed6ef
Themon645 <hilmanh707@gmail.com>
2025-11-03 18:26:12 +0700

    perbaikan latihan

--- src/main/java/id/ac/unpas/hello/world/modul6/ContohBorderLayout.java
+++ src/main/java/id/ac/unpas/hello/world/modul6/ContohBorderLayout.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package id.ac.unpas.hello.world.modul6;
 import java.awt.BorderLayout;
 import javax.swing.JButton;
@@ -23,29 +22,3 @@ public class ContohBorderLayout {
       frame.setVisible(true);
     }
 }
-=======
-package id.ac.unpas.hello.world.modul6;
-import java.awt.BorderLayout;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-
-public class ContohBorderLayout {
-    public static void main(String[] args) {
-      // 1. membuat frame
-      JFrame frame = new JFrame("Contoh BorderLayout");
-      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-      frame.setSize(400, 300);
-      // Jframe sudah menggunakan BorderLayout secara default
-
-      //2. buat dan tambahkan komponen ke 5 wilayah
-      frame.add(new JButton("NORTH"), BorderLayout.NORTH);
-      frame.add(new JButton("SOUTH"), BorderLayout.SOUTH);
-      frame.add(new JButton("EAST"), BorderLayout.EAST);
-      frame.add(new JButton("WEST"), BorderLayout.WEST);
-      frame.add(new JButton("CENTER"), BorderLayout.CENTER);
-
-      // 3. tampilkan frame
-      frame.setVisible(true);
-    }
-}
->>>>>>> 479be1fe31276ba48918b2a0a6f9a9c8340d2902
--- src/main/java/id/ac/unpas/hello/world/modul6/ContohFlowLayout.java
+++ src/main/java/id/ac/unpas/hello/world/modul6/ContohFlowLayout.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package id.ac.unpas.hello.world.modul6;
 
 import javax.swing.JButton;
@@ -32,38 +31,3 @@ public class ContohFlowLayout {
     frame.setVisible(true);
   }
 }
-=======
-package id.ac.unpas.hello.world.modul6;
-
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-
-public class ContohFlowLayout {
-  public static void main(String[] args) {
-    // 1. untuk frame (window)
-    JFrame frame = new JFrame("Contoh FlowLayout");
-    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    frame.setSize(300, 150);
-
-    // 2. untuk panel (tempat komponen)
-    //Jpanel secara default sunah menggunakan FlowLayout
-    JPanel panel = new JPanel();
-    // anda juga bisa mengaturnya secara eksplisit
-    // panel.setLayout(new FlowLayout(FlowLayout.CENTER)); //CENTER, LEFT atau RIGHT
-
-    //3. Buat dan tambahkan kkomponen ke panel
-    panel.add(new JButton("Tombol 1"));
-    panel.add(new JButton("Tombol 2"));
-    panel.add(new JButton("Tombol Tiga"));
-    panel.add(new JButton("Tombol Empat Panjang"));
-    panel.add(new JButton("Tombol 5"));
-
-    //4. tambahkan panel ke frame
-    frame.add(panel);
-
-    //5. tampilkan frame
-    frame.setVisible(true);
-  }
-}
->>>>>>> 479be1fe31276ba48918b2a0a6f9a9c8340d2902
